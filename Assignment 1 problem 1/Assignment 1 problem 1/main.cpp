6ff3400315c31556dd1d631c5f086f88463e7b01
nicohami <nicholas.hamilton@laverne.edu>
2025-10-02 11:11:46 -0700

    Add files via upload

---
+++ Assignment 1 problem 1/Assignment 1 problem 1/main.cpp
@@ -0,0 +1,34 @@
+//
+//  main.cpp
+//  Assignment 1 problem 1
+//
+//  Created by Nico  Hamilton  on 10/1/25.
+//
+
+#include <iostream>
+using namespace std;
+int main () {
+    //Declare and Initialize Variables
+    cout << "=========================================================================================" << endl;
+    cout << "                            My Fall 2025 Class Schedule          " << endl;
+    cout << "=========================================================================================" << endl;
+
+    // Display Titles
+    cout << "Day            Time         Course Code       Title             Location    Instructor " << endl;
+    
+    //Display Each Class
+    
+cout << "Mon/Wed    9:25 - 10:55am    RCS 110     College Writing A      Hoover      Prof. Huver" << endl;
+cout << "Mon/Wed    9:50 - 11:00am    MATH 250    Discrete Marth         Leo Hall    Prof. Chen" << endl;
+cout << "Tue/Thur   8:35 - 10:10am    ECON 221    Economic Analysis      Landis      Prof. Kim" << endl;
+cout << "Tue/THur   10:20 - 11:00am   CMPS 218    Programming Concepts   Founder     Dr. Goetz" << endl;
+cout << "Wed/Fri    3:00 - 4:45pm     HIST 129    U.S. History           Leo Hall    Prof. Smith" << endl;
+cout << "Wed/Fri    7:50 - 10:00pm    BIO 100     Intro to Biology       Founders    Prof. Han" << endl;
+    
+    //Display Total Units and Message
+    cout << "=========================================================================================" << endl;
+    cout << "Total units: 20" << endl;
+    cout << "=========================================================================================" << endl;
+    cout << "Have a great first semester!" << endl;
+    return 0;
+}
